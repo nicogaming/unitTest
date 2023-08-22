861c5109979a693078ac8de10808a56abc506fea
nico_jbm <n.bruunmortensen@gmail.com>
2023-08-22 18:59:49 +0200

    project files

---
+++ UnitTestProject1/UnitTestProject1/ReservationTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using UnitTestProject1;
+
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class ReservationTests
+    {
+        [TestMethod]
+        public void IsOrderedBy_OrderIsAccepted_ReturnTrue()
+        {
+            var reservation = new Reservation();
+        }
+    }
+}
---
+++ UnitTestProject1/unitTest/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Reservation 
+{
+    public Order OrderedBy { get; set; }
+
+    public bool IsOrderedBy(Order order)
+    {
+        if (order.IsAccepted)
+            return true;
+        if(OrderedBy == order)
+            return true;
+        return false;
+    }
+}
+
+
+
+
+public class Order{
+    public bool IsAccepted { get; set; }
+}
